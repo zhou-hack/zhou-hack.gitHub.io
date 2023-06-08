f65f51f1b018291fe40093e7fa3f3f91197894e4
zhou-hack <wangyimrzhou@163.com>
2023-06-08 16:10:34 +0800

    Rebuild 1

---
+++ index.js
@@ -0,0 +1,9 @@
+function redirectToBili() {
+    window.location.href = "https://space.bilibili.com/430647529";
+}
+//点头像 开Bilibili
+
+function redirectToGithub() {
+    window.location.href = "https://github.com/zhou-hack"
+}
+//Non-Used 开Github
