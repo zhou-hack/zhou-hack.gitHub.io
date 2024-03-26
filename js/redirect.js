132e68455ae1c77ea0ed23402e0023fcf07055d1
her3 <84661666+zhou-hack@users.noreply.github.com>
2024-03-26 15:03:22 +0000

    updt

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,13 @@
 function redirectToPages() {
-    console.log("Jump to CF");
-    window.location.href = "https://zhou-hack-github-io.pages.dev"
+    console.log("Jump to Pages");
+    window.location.href = "https://her3.asia/"
 }
+
+function redirectToPages() {
+    console.log("Jump to GachaPicker");
+    window.location.href = "https://gp.her3.asia/"
+}
+ 
  
 var delayTime = 2000; // 2秒
  
